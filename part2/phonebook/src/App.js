fde77a29d001c80678785de6c3cdf1da84d74a6b
Jack <ysjsz6330082@gmail.com>
2023-03-21 10:03:58 +0800

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,15 +4,18 @@ const App = () => {
     const [persons, setPersons] = useState([
         {
             name: 'Jack Young',
+            number: '123-456-7890',
             id: 1
         }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
         const personObject = {
             name: newName,
+            number: newNumber,
             id: persons.length + 1,
         }
 
@@ -21,32 +24,42 @@ const App = () => {
         } else {
             setPersons(persons.concat(personObject))
             setNewName('')
+            setNewNumber('')
         }
     }
 
-        const handlePersonChange = (event) => {
-            setNewName(event.target.value)
-        }
-
-        return (
-            <div>
-                <h2>Phonebook</h2>
-                <form onSubmit={addPerson}>
-                    <div>
-                        name: <input
-                            value={newName}
-                            onChange={handlePersonChange} />
-                    </div>
-                    <div>
-                        <button type="submit">add</button>
-                    </div>
-                </form>
-                <h2>Numbers</h2>
-                <ul>
-                    {persons.map(person => <li key={person.name}>{person.name}</li>)}
-                </ul>
-            </div>
-        )
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
     }
 
-    export default App
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input
+                        value={newName}
+                        onChange={handleNameChange} />
+                </div>
+                <div>
+                    number: <input
+                        value={newNumber}
+                        onChange={handleNumberChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default App
