c5ee08c8574ac219a4ab2f8f7205dcc66641eb9b
Jack <ysjsz6330082@gmail.com>
2023-03-21 09:53:03 +0800

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -16,33 +16,37 @@ const App = () => {
             id: persons.length + 1,
         }
 
-        setPersons(persons.concat(personObject))
-        setNewName('')
+        if (persons.some(person => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            setPersons(persons.concat(personObject))
+            setNewName('')
+        }
     }
 
-    const handlePersonChange = (event) => {
-        setNewName(event.target.value)
+        const handlePersonChange = (event) => {
+            setNewName(event.target.value)
+        }
+
+        return (
+            <div>
+                <h2>Phonebook</h2>
+                <form onSubmit={addPerson}>
+                    <div>
+                        name: <input
+                            value={newName}
+                            onChange={handlePersonChange} />
+                    </div>
+                    <div>
+                        <button type="submit">add</button>
+                    </div>
+                </form>
+                <h2>Numbers</h2>
+                <ul>
+                    {persons.map(person => <li key={person.name}>{person.name}</li>)}
+                </ul>
+            </div>
+        )
     }
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input
-                    value={newName}
-                    onChange={handlePersonChange}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <ul>
-                {persons.map(person => <li key={person.name}>{person.name}</li>)}
-            </ul>
-        </div>
-    )
-}
-
-export default App
+    export default App
