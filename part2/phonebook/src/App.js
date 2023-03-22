eeaae1a50de35051060aa93dc953d310350966c5
Jack <ysjsz6330082@gmail.com>
2023-03-22 10:35:03 +0800

    2.12: The Phonebook step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -24,15 +24,19 @@ const App = () => {
         const personObject = {
             name: newName,
             number: newNumber,
-            id: persons.length + 1,
         }
 
         if (persons.some(person => person.name === newName)) {
             alert(`${newName} is already added to phonebook`)
         } else {
-            setPersons(persons.concat(personObject))
-            setNewName('')
-            setNewNumber('')
+            axios
+                .post('http://localhost:3001/persons', personObject)
+                .then(response => {
+                    setPersons(persons.concat(response.data))
+                    setNewName('')
+                    setNewNumber('')
+                })
+
         }
     }
 
